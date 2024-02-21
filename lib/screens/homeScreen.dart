9eff011217cc84fbe886fa28e17bbb9d6dc47a06
Abraham <abrahamperezquiroz0@gmail.com>
2024-02-21 12:35:00 -0600

    Navigator.push home screen

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:practica_3/screens/infinite_list.dart';
+import 'package:practica_3/screens/inputs.dart';
+import 'package:practica_3/screens/notification.dart';
 import 'package:practica_3/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,23 +17,41 @@ class HomeScreen extends StatelessWidget {
           ListView(
             children:  <Widget> [
               ListTile(
-                title: Text('Sulfuro', style: AppTheme.lightTheme.textTheme.headlineLarge),
-                subtitle: Text('De cloruro', style: AppTheme.lightTheme.textTheme.bodySmall),
+                title: Text('Entradas', style: AppTheme.lightTheme.textTheme.headlineLarge),
+                subtitle: Text('Recuperar informacion del textfold', style: AppTheme.lightTheme.textTheme.bodySmall),
                 leading: const Icon(Icons.input_rounded, color: AppTheme.primaryColor),
+                onTap: () {
+                  final ruta1 = MaterialPageRoute(builder: (context){
+                    return const inputs();
+                  });
+                  Navigator.push(context, ruta1);
+                },
                 trailing: const Icon(Icons.arrow_right_alt_outlined, color: AppTheme.secondaryColor,),
               ),
               Divider(),
               ListTile(
-                title:  Text('Cloruro', style: AppTheme.lightTheme.textTheme.headlineLarge),
-                subtitle:  Text('de sodio', style: AppTheme.lightTheme.textTheme.bodySmall),
+                title:  Text('Lista infinita', style: AppTheme.lightTheme.textTheme.headlineLarge),
+                subtitle:  Text('Recuperar elementos', style: AppTheme.lightTheme.textTheme.bodySmall),
                 leading: const Icon(Icons.list_alt_sharp, color: AppTheme.primaryColor),
+                 onTap: () {
+                  final ruta2 = MaterialPageRoute(builder: (context){
+                    return const InfiniteList();
+                  });
+                  Navigator.push(context, ruta2);
+                },
                 trailing: const Icon(Icons.arrow_right_alt_outlined, color: AppTheme.secondaryColor,),
               ),
               Divider(),
               ListTile(
-                title: Text('Acido', style: AppTheme.lightTheme.textTheme.headlineLarge),
-                subtitle: Text('Sulfurico', style: AppTheme.lightTheme.textTheme.bodySmall),
+                title: Text('Notificaciones', style: AppTheme.lightTheme.textTheme.headlineLarge),
+                subtitle: Text('Mensajes', style: AppTheme.lightTheme.textTheme.bodySmall),
                 leading: const Icon(Icons.notification_important, color: AppTheme.primaryColor),
+                 onTap: () {
+                  final ruta3 = MaterialPageRoute(builder: (context){
+                    return const Notifications();
+                  });
+                  Navigator.push(context, ruta3);
+                },
                 trailing: const Icon(Icons.arrow_right_alt_outlined, color: AppTheme.secondaryColor,),
               )
           ],
